d729a93ea04f8f77b58b74f7b5baef22450684ad
Ittirit Thipphinit <itchampclub@gmail.com>
2018-11-16 13:13:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,8 +6,8 @@ require_once('./CurrencyExchange.php');
 require_once('./Zomato.php');
 require_once('./GooglePlace.php');
 
-$channelAccessToken = 'LYuQSwsxigKYSql4Ad3VdLsoCjmPbplcKJiT5XIDTeCdFxVGO0lC9uvGFcnmOEpc+Ams035JZ/+PfReMjlTYSidnX+GvrLH3T1QqRx/R4CxNU4EBw3uD+0iR98IvmyU9Udl8hge9HAPn/UpeYumQwwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '03371616390b4cb96139412c5ce45d53';
+$channelAccessToken = '1B8pMUtZdLgdebgTuRrxV3YirCQv91mbXGnxvlTbX7Cxn471Fs0bBgwGVpedxnPKm7tZUWxnMrT2NqCBCLAG8L7r6vtYoZwb3iqRvYr3BZGrZX/mRNFG8lzNbLr5CHO4PWfTicerD5PVHYjC8mpQ4wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '69b2d81ee6e8ff48d2cacdc8c7d8c337';
 date_default_timezone_set('Asia/Jakarta');
 
 $client 		= new LINEBotTiny($channelAccessToken, $channelSecret);
